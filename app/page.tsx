77960a05105dc86cd302db7dbdb521906314063b
Bluebear645 <bluebear060405@naver.com>
2023-05-24 04:55:09 +0900

    급식 UI 완성

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
 import Image from 'next/image';
 import styles from './page.module.css';
 import axios from 'axios';
+import xml2js from 'xml2js';
 
 import { Noto_Sans_KR } from 'next/font/google';
 import { json } from 'stream/consumers';
+import { exp } from 'mathjs';
 
 // If loading a variable font, you don't need to specify the font weight
 const noto_sans = Noto_Sans_KR({
@@ -13,27 +15,15 @@ const noto_sans = Noto_Sans_KR({
 });
 
 // If loading a variable font, you don't need to specify the font weight
-const noto_sans_bold = Noto_Sans_KR({
+const noto_sans_light = Noto_Sans_KR({
     subsets: ['latin'],
     display: 'swap',
-    weight: '700',
+    weight: '400',
 });
 
 const today = new Date();
 
 export default function Home() {
-    async function fetchData() {
-        const url = `https://school-api.xyz/api/high/J100000801?date=${today.getDate()}&allergy=hidden`;
-        try {
-            const response = await axios.get(url);
-            const data = response.data;
-            const lunch = data.menu[0].lunch.map((item: string) => item.replace(/^j/, ''));
-            return lunch;
-        } catch (error) {
-            return error;
-        }
-    }
-    const lunchmenu = fetchData();
     return (
         <div>
             <img className='main-logo' src='/icon.svg' width='50vw' />
@@ -47,16 +37,40 @@ export default function Home() {
                     <div className='lunch'>
                         {lunchmenu.then((menu: string[]) =>
                             menu.map((item: string, index: number) => (
-                                <span className={`lunch-text ${noto_sans_bold.className}`}>
+                                <span className={`lunch-text ${noto_sans.className}`}>
                                     {index + 1}. {item}
                                     <br></br>
                                 </span>
                             ))
                         )}
+                        <span className={`kcal-text ${noto_sans_light.className}`}>{fetchKcal()}</span>
                     </div>
                 </div>
+                <div className='img-icon'></div>
+                <span className={`img-text ${noto_sans_light.className}`}>사진</span>
             </div>
             {/* <div className='card'></div> */}
         </div>
     );
+}
+
+export async function fetchData() {
+    const url = `https://school-api.xyz/api/high/J100000801?date=${today.getDate()}&allergy=hidden`;
+    try {
+        const response = await axios.get(url);
+        const data = response.data;
+        const lunch = data.menu[0].lunch.map((item: string) => item.replace(/^j/, ''));
+        return lunch;
+    } catch (error) {
+        return error;
+    }
+}
+const lunchmenu = fetchData();
+
+export async function fetchKcal() {
+    const res = await fetch(
+        `https://open.neis.go.kr/hub/mealServiceDietInfo?KEY=c814eb81cdef46fda17b8aa5c0b04d97&TYPE=json&ATPT_OFCDC_SC_CODE=J10&SD_SCHUL_CODE=7530525&MLSV_YMD=20230523`
+    );
+    const data = await res.json();
+    return data.mealServiceDietInfo[1].row[0].CAL_INFO;
 }
