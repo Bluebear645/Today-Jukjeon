c66faa1e1d4432d05d75ed7c55953498afc8f7e1
Bluebear645 <bluebear060405@naver.com>
2023-08-24 04:07:00 +0900

    UseEffect 사용설정, Componenet 분리

---
+++ app/lunch.tsx
@@ -0,0 +1,69 @@
+'use client';
+import React, { useState, useEffect } from 'react';
+import { Noto_Sans_KR } from 'next/font/google';
+
+// If loading a variable font, you don't need to specify the font weight
+const noto_sans = Noto_Sans_KR({
+    subsets: ['latin'],
+    display: 'swap',
+    weight: '500',
+});
+
+// If loading a variable font, you don't need to specify the font weight
+const noto_sans_light = Noto_Sans_KR({
+    subsets: ['latin'],
+    display: 'swap',
+    weight: '400',
+});
+
+//날짜 정의
+const today = new Date();
+const year = today.getFullYear();
+const month = today.getMonth() + 1;
+const day = today.getDate();
+
+function Lunch() {
+    const [lunchmenu, setLunchmenu] = useState<string[]>([]);
+    const [kcal, setKcal] = useState<string>('');
+
+    useEffect(() => {
+        async function fetchData() {
+            const YMD = `${year}${month < 10 ? '0' + month : month}${day < 10 ? '0' + day : day}`;
+            const res = await fetch(
+                `https://open.neis.go.kr/hub/mealServiceDietInfo?KEY=c814eb81cdef46fda17b8aa5c0b04d97&TYPE=json&ATPT_OFCDC_SC_CODE=J10&SD_SCHUL_CODE=7530525&MLSV_YMD=${YMD}`
+            );
+            const data = await res.json();
+            const list = data.mealServiceDietInfo[1].row[0].DDISH_NM;
+            const menu = list.split('<br/>').map((item: string) =>
+                item
+                    .replace(/^j/, '')
+                    .replace(/\(.*?\)/g, '')
+                    .trim()
+            );
+            setLunchmenu(menu);
+            setKcal(data.mealServiceDietInfo[1].row[0].CAL_INFO);
+        }
+        fetchData();
+    }, []);
+
+    return (
+        <div className='box'>
+            <div className='date'>
+                <span className={`date-text ${noto_sans.className}`}>
+                    {today.getFullYear()}.{today.getMonth() + 1}.{today.getDate()} {new Intl.DateTimeFormat('ko-KR', { weekday: 'long' }).format(today)}
+                </span>
+            </div>
+            <div className='lunch'>
+                {lunchmenu.map((item: string, index: number) => (
+                    <span className={`lunch-text ${noto_sans.className}`}>
+                        {index + 1}. {item}
+                        <br></br>
+                    </span>
+                ))}
+                <span className={`kcal-text ${noto_sans_light.className}`}>{kcal}</span>
+            </div>
+        </div>
+    );
+}
+
+export default Lunch;
--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,7 @@ import Image from 'next/image';
 import styles from './page.module.css';
 import axios from 'axios';
 import xml2js from 'xml2js';
+import Lunch from './lunch';
 const fs = require('fs');
 
 import { Noto_Sans_KR } from 'next/font/google';
@@ -23,69 +24,17 @@ const noto_sans_light = Noto_Sans_KR({
 });
 
 //날짜 정의
-const today = new Date();
-const year = today.getFullYear();
-const month = today.getMonth() + 1;
-const day = today.getDate();
 
 export default function Home() {
     return (
         <div>
             <img className='main-logo' src='/icon.svg' width='50vw' />
             <div className='main-border'>
-                <div className='box'>
-                    <div className='date'>
-                        <span className={`date-text ${noto_sans.className}`}>
-                            {today.getFullYear()}.{today.getMonth() + 1}.{today.getDate()} {new Intl.DateTimeFormat('ko-KR', { weekday: 'long' }).format(today)}
-                        </span>
-                    </div>
-                    <div className='lunch'>
-                        {lunchmenu.then((menu: string[]) =>
-                            menu.map((item: string, index: number) => (
-                                <span className={`lunch-text ${noto_sans.className}`}>
-                                    {index + 1}. {item}
-                                    <br></br>
-                                </span>
-                            ))
-                        )}
-                        <span className={`kcal-text ${noto_sans_light.className}`}>{fetchKcal()}</span>
-                    </div>
-                </div>
+                <Lunch />
                 {/* <div className='img-icon'></div>
                 <span className={`img-text ${noto_sans_light.className}`}>사진</span> */}
             </div>
             {/* <div className='card'></div> */}
         </div>
     );
-}
-
-async function LoadData(type: string) {}
-const databuffer = fs.readFileSync('db.json');
-const dataJSON = databuffer.toString();
-const data = JSON.parse(dataJSON);
-if (data.updatedAt === today.getDate()) {
-}
-async function fetchData() {
-    const YMD = `${year}${month < 10 ? '0' + month : month}${day < 10 ? '0' + day : day}`;
-    const res = await fetch(
-        `https://open.neis.go.kr/hub/mealServiceDietInfo?KEY=c814eb81cdef46fda17b8aa5c0b04d97&TYPE=json&ATPT_OFCDC_SC_CODE=J10&SD_SCHUL_CODE=7530525&MLSV_YMD=${YMD}`
-    );
-    const data = await res.json();
-    const list = data.mealServiceDietInfo[1].row[0].DDISH_NM;
-    return list.split('<br/>').map((item: string) =>
-        item
-            .replace(/^j/, '')
-            .replace(/\(.*?\)/g, '')
-            .trim()
-    );
-}
-const lunchmenu = fetchData();
-
-async function fetchKcal() {
-    const YMD = `${year}${month < 10 ? '0' + month : month}${day < 10 ? '0' + day : day}`;
-    const res = await fetch(
-        `https://open.neis.go.kr/hub/mealServiceDietInfo?KEY=c814eb81cdef46fda17b8aa5c0b04d97&TYPE=json&ATPT_OFCDC_SC_CODE=J10&SD_SCHUL_CODE=7530525&MLSV_YMD=${YMD}`
-    );
-    const data = await res.json();
-    return data.mealServiceDietInfo[1].row[0].CAL_INFO;
 }
